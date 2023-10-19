9eaf7f15534d9fc5f2e1736c8402e8cb0ca6d27c
kajal-2011 <kajalrekha888@gmail.com>
2023-10-19 08:50:24 +0600

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer p-10 bg-gray-900 container mx-auto rounded-b-lg border-t border-teal-900 border-dashed lg:max-w-4xl">
-      <p className="text-center text-teal-600 text-sm">
+      <p className="text-center text-teal-600 text-md">
         &copy; {new Date().getFullYear()} Kajal Rekha. All rights reserved
       </p>
     </footer>
